afb0c79343d4a55400745cd9718eb5959d7721f6
edwinwijaya94 <edwinwijaya94@yahoo.com>
2016-02-14 19:07:05 +0700

    add form validation

--- stok.php
+++ stok.php
@@ -13,39 +13,39 @@
           <a class="add-btn waves-effect waves-light btn modal-trigger green darken-1" href="#add-modal">Tambah Baru</a>
           <!-- Modal Structure -->
           <div id="add-modal" class="modal modal-fixed-footer">
-            <div class="modal-content">
+            <div class="modal-content" style="height:100%">
               <h4>Tambah ATK Baru</h4>
               <div class="row">
-                <form class="col s12">
+                <form id="add-form" class="col s12" action="">
                   <div class="row">
                     <div class="input-field col s6">
-                      <input id="id-atk" type="text" class="validate">
+                      <input disabled id="id-atk" type="text" class="validate" required="" aria-required="true">
                       <label for="id-atk">ID ATK</label>
                     </div>
                   </div> 
                   <div class="row"> 
                     <div class="input-field col s6">
-                      <input id="jenis-atk" type="text" class="validate">
+                      <input id="jenis-atk" type="text" class="validate" required="" aria-required="true">
                       <label for="jenis-atk">Jenis ATK</label>
                     </div>
                   </div>
                   <div class="row">
                     <div class="input-field col s6">
-                      <input id="jumlah" type="text" class="validate">
+                      <input id="jumlah" type="text" class="validate" required="" aria-required="true">
                       <label for="jumlah">Jumlah</label>
                     </div>
                   </div>
                   <div class="row">
                     <div class="input-field col s6">
-                      <input id="stok-min" type="text" class="validate">
+                      <input id="stok-min" type="text" class="validate" required="" aria-required="true">
                       <label for="stok-min">Stok Minimum</label>
                     </div>
                   </div>
+                  <div class="row">
+                    <button class="tambahkan btn waves-effect waves-light green darken-1" type="submit">Tambahkan</button>
+                  </div>
                 </form>
               </div>
-            </div>
-            <div class="modal-footer">
-              <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Tambahkan</a>
             </div>
           </div>
         </div>
@@ -82,39 +82,39 @@
             </table>
             <!-- Edit-Modal-->
               <div id="edit-modal" class="modal modal-fixed-footer">
-                <div class="modal-content">
-                  <h4>Perbarui Data ATK</h4>
+                <div class="modal-content" style="height:100%">
+                  <h4>Ubah Data ATK</h4>
                   <div class="row">
-                    <form class="col s12">
+                    <form id="edit-form" class="col s12" action="">
                       <div class="row">
                         <div class="input-field col s6">
-                          <input id="id-atk" type="text" class="validate">
+                          <input disabled id="id-atk" type="text" class="validate" required="" aria-required="true">
                           <label class="edit-label" for="id-atk">ID ATK</label>
                         </div>
                       </div> 
                       <div class="row"> 
                         <div class="input-field col s6">
-                          <input id="jenis-atk" type="text" class="validate">
+                          <input id="jenis-atk" type="text" class="validate" required="" aria-required="true">
                           <label class="edit-label" for="jenis-atk">Jenis ATK</label>
                         </div>
                       </div>
                       <div class="row">
                         <div class="input-field col s6">
-                          <input id="jumlah-atk" type="text" class="validate">
-                          <label class="edit-label" for="jumlah">Jumlah</label>
+                          <input id="jumlah-atk" type="text" class="validate" required="" aria-required="true">
+                          <label class="edit-label" for="jumlah-atk">Jumlah</label>
                         </div>
                       </div>
                       <div class="row">
                         <div class="input-field col s6">
-                          <input id="stok-min" type="text" class="validate">
+                          <input id="stok-min" type="text" class="validate" required="" aria-required="true">
                           <label class="edit-label" for="stok-min">Stok Minimum</label>
                         </div>
                       </div>
+                      <div class="row">
+                        <button class="ubah btn waves-effect waves-light orange darken-1" type="submit">Ubah</button>
+                      </div>
                     </form>
                   </div>
-                </div>
-                <div class="modal-footer">
-                  <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Perbarui</a>
                 </div>
               </div>
             <!-- Delete-Modal-->
@@ -124,8 +124,8 @@
                   <p>Anda yakin akan menghapus data ini ?</p>
                 </div>
                 <div class="modal-footer">
-                  <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Hapus</a>
-                  <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Batal</a>
+                  <div><a href="#!" class=" btn waves-effect waves-light red darken-4" >Hapus</a></div>
+                  <div><a href="#!" class=" btn waves-effect waves-light green darken-4" style="margin-right:20px">Batal</a></div>
                 </div>
               </div>
 
@@ -161,8 +161,8 @@
           });
           //auto-increment ID for new record
           var newId = "ATK" + (parseInt($("#tabel-stok > tbody > tr:last-child> td").eq(0).text().substring(3))+1);
-          console.log(newId);
           $("#add-modal").find("#id-atk").val(newId);
+          
         });
       </script>
     </body>
