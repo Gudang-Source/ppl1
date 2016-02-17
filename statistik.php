edd91686f873938fb2bfb3acef4aa37333b51574
levanjipm <myc.glace@gmail.com>
2016-02-17 21:47:00 +0700

    bingung mindahin array

--- statistik.php
+++ statistik.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
   <html>
     <head>
-      <?php include('header.html'); ?>
+      <?php include('header.html'); 
+			require_once("database.php");
+		?>
       <title>SISTER | Statistik</title>
     </head>
 
@@ -15,9 +17,31 @@
       </div>
 
       <?php include('footer.html'); ?>
-
+	
       <!--js-->
       <script type="text/javascript">
+	    <?php
+			include ("Connect.php");
+			global $conn;
+			
+			$query = "SELECT nama, COUNT(nama) FROM user JOIN pemakaian ON user.uid = pemakaian.p_uid GROUP BY nama";
+			
+			$rquery = mysqli_query($conn, $query);
+			$row = mysqli_fetch_array($rquery, MYSQLI_NUM);
+			$rowcount = mysqli_num_rows($rquery);
+			$arrcount = $rowcount * 2;
+	    ?>
+		
+		var arr = [<?php echo '"'.implode('","', $row).'"' ?>];
+		var arr_labels;
+		var i;
+		/*for (i = 0; i < 3; i++){
+			arr_labels[i] = arr[i*2];
+		} --> ini yang bikin ga keluar grafik nya */ 
+		
+		var arr_series;
+		
+		var i = 0;
         var data = {
           labels: ['Edwin', 'Levanji', 'Raka', 'Nitho'],
           series: [
