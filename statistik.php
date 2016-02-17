ef367d2ac3c0ac16ec55c46f3a2c4f87e978abf1
edwinwijaya94 <edwinwijaya94@yahoo.com>
2016-02-17 19:37:06 +0700

    add frontend statistik

--- statistik.php
+++ statistik.php
@@ -7,13 +7,43 @@
 
     <body>
       <?php include('navbar.html'); ?>
-
+      <div class="container">
+        <div class="col s12" style="font-size:25px; text-align:center; margin-top:150px">Pemakaian ATK Setiap Pengguna</div>
+        <div class="row" style="text-align:center">
+          <div class="ct-chart col s12" style="margin-top:50px; display:inline-block"></div>
+        </div>
+      </div>
 
       <?php include('footer.html'); ?>
 
       <!--js-->
       <script type="text/javascript">
-        document.getElementById("statistik").className += "active";
+        var data = {
+          labels: ['Edwin', 'Levanji', 'Raka', 'Nitho'],
+          series: [
+            [10, 4, 3, 7]
+          ]
+        };
+
+        var options = {
+          seriesBarDistance: 10,
+          axisY:{
+            onlyInteger: true
+          }
+        };
+
+        var responsiveOptions = [
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
+        ];
+        new Chartist.Bar('.ct-chart', data, options, responsiveOptions);
+      
       </script>
     </body>
   </html>
